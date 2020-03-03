74a0e002bc8d5dd6a6da7a1defb6ab4ecf71ef0a
unknown <pyojs95@pusan.ac.kr>
2020-03-03 23:56:03 +0900

    20200303

---
+++ n_and_m_2.py
@@ -0,0 +1,31 @@
+N, M = map(int, input().split())
+
+# 조합을 만들기 위한 함수
+def cob(a):
+    # 쓰는 경우 1, 안쓰는 경우 0
+    c = [1, 0]
+    # 반복 완료 시 결과 저장 / 순서대로 저장하는 것도 필요
+    if a == N:
+        if sum(check) == M:
+            r = [0] * M
+            k = 0
+            for i in range(len(check)):
+                if check[i]:
+                    r[k] = arr[i]
+                    k += 1
+            result.append(r)
+    else:
+        for i in range(2):
+            check[a] = c[i]
+            cob(a+1)
+
+# 1~N이 있는 배열 생성
+arr = [i for i in range(1, N+1)]
+# check로 사용 여부 결정 예정
+check = [0] * N
+result = []
+cob(0)
+
+# 출력
+for k in range(len(result)):
+    print(*result[k])
---
+++ work_div.py
@@ -0,0 +1,27 @@
+# 겹치지 않게 일을 분배하는 함수(순열 생각)
+def per(a, s):
+    global max_per
+    # 확률을 곱할수록 작아짐으로 이미 구한 값보다 작은 경우 바로 종료
+    if max_per < s:
+        if a == N:
+            # 더 높은 확률이 나온 경우 결과 수정
+            if s > max_per:
+                max_per = s
+        else:
+            for i in range(N):
+                if check[i] == 0:
+                    check[i] = a+1
+                    per(a+1, s * arr[i][a]/100)
+                    check[i] = 0
+                    
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    check = [0]*N
+    max_per = 0
+    per(0, 1)
+    # 퍼센트로 맞추기 위해 100을 곱함
+    max_per *= 100
+
+    print('#{} {:.6f}'.format(t, max_per))
