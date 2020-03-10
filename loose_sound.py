f0d12d6ddce1d880387a0f6b439428ed64d058ce
unknown <pyojs95@pusan.ac.kr>
2020-03-11 02:26:00 +0900

    20200311

---
+++ loose_sound.py
@@ -0,0 +1,15 @@
+T = int(input())
+for tc in range(1, T+1):
+    sound = list(input())
+    H = int(input())
+    location = list(map(int, input().split()))
+    # -을 추가할 위치를 세는 배열 생성
+    location_cnt = [0]*(len(sound)+1)
+    for n in location:
+        location_cnt[n] += 1
+    # 뒤쪽부터 -을 추가
+    for i in range(len(location_cnt)-1, -1, -1):
+        if location_cnt[i]:
+            sound.insert(i, '-'*location_cnt[i])
+    # join을 통해 다시 합해서 출력
+    print('#{} {}'.format(tc,''.join(sound)))
