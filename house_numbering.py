e04c029be3abe2779538a9266a5e68e2111f4890
unknown <pyojs95@pusan.ac.kr>
2020-04-18 22:22:56 +0900

    20200418

--- house_numbering.py
+++ house_numbering.py
@@ -36,3 +36,37 @@ print(len(result_list))
 result_list.sort()
 for n in result_list:
     print(n)
+
+# def boundary(x, y):
+#     if x<0 or y<0 or x>N-1 or y>N-1 or arr[y][x] == 0:
+#         return False
+#     return True
+
+# def dfs(x, y):
+#     global temp_result
+#     for d in range(4):
+#         if boundary(x+dx[d], y+dy[d]) and visited[y+dy[d]][x+dx[d]] == 0:
+#             visited[y+dy[d]][x+dx[d]] = 1
+#             temp_result += 1
+#             dfs(x+dx[d], y+dy[d])
+
+# N = int(input())
+# arr = [list(map(int,input())) for _ in range(N)]
+# visited = [[0 for _ in range(N)] for _ in range(N)]
+
+# dx = [1, -1, 0, 0]
+# dy = [0, 0, 1, -1]
+
+# result_list = []
+# for j in range(N):
+#     for i in range(N):
+#         if arr[j][i] == 1 and visited[j][i] == 0:
+#             temp_result = 1
+#             visited[j][i] = 1
+#             dfs(i, j)
+#             result_list.append(temp_result)
+# result_list.sort()
+# cnt = len(result_list)
+# print(cnt)
+# for i in range(cnt):
+#     print(result_list[i])
